5b44e262056e33ac3c8e1a959373ebd383860087
[Taiyo] [Okano] <[taiyo.okano@my.bdsc.school.nz]>
2026-05-20 09:30:42 +1200

    I wrote main menu

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-print("I have changed this line")
-print("And added this one")
+print("Welcome to a New To-do list!")
+main_menu = input("Please choose your option below by a number \n[1: View Lists, 2: Add Task, 3: Remove a Task, 4: Mark Complete, 5: Exit]\n:  ")
