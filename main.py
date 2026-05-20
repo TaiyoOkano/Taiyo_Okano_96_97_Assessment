cc108f6f63b81e22a78b5d06cf8a97578e6e924c
[Taiyo] [Okano] <[taiyo.okano@my.bdsc.school.nz]>
2026-05-21 11:00:45 +1200

    I wrote while statement, but I need to double check how it works

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("Welcome to a New To-do list!")
-main_menu = input("Please choose your option below by a number \n[1: View Lists, 2: Add Task, 3: Remove a Task, 4: Mark Complete, 5: Exit]\n:  ")
+main_menu = input("Please choose your option below list by a number \n[1: View Lists, 2: Add Task, 3: Remove a Task, 4: Mark Complete, 5: Exit]\n:  ")
+#This line 1~2 are main menu. Users will choose their option from the list.
+
+user_choice = True
+while user_choice == 5:
+    print("Wrong choice. Try again.")
