43dda908468e1a2044e6baf8fa58dea52ca882e2
Taiyo Okano <wasabioniku@gmail.com>
2026-05-26 17:02:41 +1200

    Fixed Valueerror

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ options = [1,2,3,4,5]
 while True:
     try:
 
-        print()#This print function creates a blank line that might help clear reading for users.
+        print("\n")#This print function creates a blank line that might help clear reading for users.
         print("[1: View Lists, 2: Add Task, 3: Remove a Task, 4: Mark Complete, 5: Exit]")#This line is main menu options. Users will choose their option from the list.
         user_input = int(input("Please choose your option from the list above by a number \n: "))
         if user_input in options:
-                
+            if user_input == 1:
+                print("Viewing lists (not implemented yet).")
+            elif user_input == 2:
+                print("Add Task (not implemented yet).")
+            elif user_input == 3:
+                print("Remove a Task (not implemented yet).")
+            elif user_input == 4:
+                print("Mark Complete (not implemented yet).")
+            elif user_input == 5:
+                print("Exiting. Goodbye!")
+                break
         else:
             print("Your option isn't available. Please try again.")
     except ValueError:
