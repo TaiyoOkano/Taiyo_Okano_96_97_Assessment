72e3853eb38a00725d4b15cb220a5946d0e52bdb
Taiyo Okano <wasabioniku@gmail.com>
2026-05-28 10:56:40 +1200

    Fixed add task's input.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 print("Welcome to a New To-do list!")
 
-tasks = []#This is an empty list that will be used to store the tasks that users add to their to-do list. 
+tasks = []#This is an empty list that will be used to store the tasks that users add to their to-do list.
 def View_Lists():
     print("Here is your current to-do list.")
     print(f"{tasks}")
 
 def Add_Task():
     print("Add Task.")
-    new_task = input("Enter a task you want to add: ")
-    tasks.append(new_task)
-    print(f"Your task '{new_task}' has been added.")
+    while True:
+        new_task = input("Enter a task you want to add: ").strip()
+        if new_task == "":
+            print("Your task cannot be empty or just spaces. Please try again.")
+        else:
+            tasks.append(new_task)
+            print(f"Your task '{new_task}' has been added.")
+            break
+
 
 def Remove_Task():
     print("Remove a Task.")
-    del_task = input("Enter a task you want to remove: ") 
+    del_task = input("Enter a task you want to remove: ")
     tasks.remove(del_task)
     print(f"Your task '{del_task}' has been removed.")
 
@@ -30,7 +36,6 @@ while True:
         user_input = int(input("Please choose your option from the list above by a number \n: "))
         if user_input in options:
             if user_input == 1:
-<<<<<<< HEAD
                 View_Lists()
             elif user_input == 2:
                 Add_Task()
@@ -40,17 +45,6 @@ while True:
                 Mark_Complete()
             elif user_input == 5:
                 print("Goodbye.")
-=======
-                print("Viewing lists (not implemented yet).")
-            elif user_input == 2:
-                print("Add Task (not implemented yet).")
-            elif user_input == 3:
-                print("Remove a Task (not implemented yet).")
-            elif user_input == 4:
-                print("Mark Complete (not implemented yet).")
-            elif user_input == 5:
-                print("Exiting. Goodbye!")
->>>>>>> 43dda908468e1a2044e6baf8fa58dea52ca882e2
                 break
         else:
             print("Your option isn't available. Please try again.")
