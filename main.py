3d7a391d247318c12aaf8b75723d2ec5940355ed
Taiyo Okano <wasabioniku@gmail.com>
2026-06-06 21:24:13 +1200

    Finished_Task3(Implement Category Deletion)_and almost Task4(Upgrade View Lists with Auto-Display )

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def save_tasks():#This function serialize the dictionary.
         json.dump(tasks, f)
 
 def load_tasks():#This function deserialize the dictionary.
-    try:#This try-except block would create a new To-Do list if there is no saved_tasks.json file.
+    try:#This try-except gblock would create a new To-Do list if there is no saved_tasks.json file.
         global tasks
         with open("saved_tasks.json", "r") as f:
             tasks = json.load(f)
@@ -34,9 +34,10 @@ def View_Lists():#This function is for users to view thier current To-Do list.
 
 def Add_Task():#This function is for users to add a task to thier To-Do list.
     print("Add Task.")
+    View_Lists()
     category = input("Enter a category (e.g. work, personal)\n: ").strip().lower()
     if category == "":#This if statement is for if category is empty or just spaces.
-        print("Category cannot be empty or just spaces.")
+        print("Category cannot be empty or just spaces. Please try again.")
         return
     
     while True:
@@ -53,30 +54,61 @@ def Add_Task():#This function is for users to add a task to thier To-Do list.
             else:
                 tasks[category].append(new_task)
                 print(f"Your task '{new_task}' has been added to [{category}].")
+                View_Lists()
                 break
 
-def Remove_Task():#This function is for users to remove a task from thier To-Do list.
-    print("Remove a Task.")
+def Remove_List():#This function is for users to remove a task from thier To-Do list.
+    print("Remove a List.")
+    while True:      
+        try:#This try-except block prevents the program from crashing when users enter invalid input.
+            del_ask = int(input("Do you want to remove a category or a task? Enter by 1 or 2 [1: Category, 2: Task]\n: "))
+            if del_ask == 1:
+                Remove_Category()
+                break        
+            elif del_ask == 2:
+                Remove_Task()
+                break
+            else:
+                print("Your option isn't available. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a number.\n")
+
+
+def Remove_Category():  
+    View_Lists() 
+    del_category = input("Enter the category you want to remove\n: ").strip().lower()
+    if del_category in tasks:
+        del tasks[del_category]
+        print(f"Your category [{del_category}] has been removed.")
+    else:
+        print(f"Your category [{del_category}] doesn't exist. Please try again.\n")
+        
+        
+
+def Remove_Task():
+    View_Lists()
     category = input("Enter the category of the task you want to remove\n: ").strip().lower()
     if category not in tasks:
         print(f"Your category [{category}] doesn't exist. Please try again.")
         return
-    
-    del_task = input("Enter a task you want to remove: ").strip().lower()
-    #Those if statements are for if the task is in the list, it will be removed. 
-    #If the task with "(Complete)" is in the list, it will also be removed. 
-    #If neither of them is in the list, it will print a message that the task is not found.
-    if del_task in tasks[category]:
-        tasks[category].remove(del_task)
-        print(f"Your task '{del_task}' has been removed from [{category}].")
-    elif del_task + "(Complete)" in tasks[category]:
-        tasks[category].remove(del_task + "(Complete)")
-        print(f"Your task '{del_task}' has been removed from [{category}].")
     else:
-        print(f"Task '{del_task}' not found in [{category}].")
+        del_task = input("Enter a task you want to remove: ").strip().lower()
+        #Those if statements are for if the task is in the list, it will be removed. 
+        #If the task with "(Complete)" is in the list, it will also be removed. 
+        #If neither of them is in the list, it will print a message that the task is not found.
+        if del_task in tasks[category]:
+            tasks[category].remove(del_task)
+            print(f"Your task '{del_task}' has been removed from [{category}].")
+        elif del_task + "(Complete)" in tasks[category]:
+            tasks[category].remove(del_task + "(Complete)")
+            print(f"Your task '{del_task}' has been removed from [{category}].")
+        else:
+            print(f"Task '{del_task}' not found in [{category}].")
 
+        
 def Mark_Complete():#This function is for users to mark a task as complete in their To-Do list.
     print("Mark Complete.")
+    View_Lists()
     category = input("Enter the category of the task you want to mark as complete\n: ").strip().lower()
     if category not in tasks:
         print(f"Your category [{category}] doesn't exist. Please try again.")
@@ -96,7 +128,7 @@ options = [1,2,3,4,5]
 while True:#This while loop is for the main menu. It will keep running until users choose to exit the program by entering 5.
     try:#This try-except block prevents the program from crashing when users enter invalid input.
         print("\n")#This print function creates a blank line that might help clear reading for users.
-        print("[1: View Lists, 2: Add Task, 3: Remove a Task, 4: Mark Complete, 5: Exit]")#This line is main menu options. Users will choose their option from the list.
+        print("[1: View Lists, 2: Add Task, 3: Remove a List, 4: Mark Complete, 5: Exit]")#This line is main menu options. Users will choose their option from the list.
         user_input = int(input("Please choose your option from the list above by a number \n: "))
         if user_input in options:
             if user_input == 1:
@@ -104,7 +136,7 @@ while True:#This while loop is for the main menu. It will keep running until use
             elif user_input == 2:
                 Add_Task()
             elif user_input == 3:
-                Remove_Task()
+                Remove_List()
             elif user_input == 4:
                 Mark_Complete()
             elif user_input == 5:
@@ -114,4 +146,4 @@ while True:#This while loop is for the main menu. It will keep running until use
         else:
             print("Your option isn't available. Please try again.")
     except ValueError:
-        print("Invalid input. Please enter a number.")
+        print("Invalid input. Please enter a number.\n")
